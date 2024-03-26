76e974dff7f19f7e2cbbf035f44e249d1969db76
vagrant <arman.augusto@gmail.com>
2024-03-25 21:03:51 -0700

    added some redirecting actions

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,14 @@
 class MainController < ApplicationController
   def index
+    # render('index')
   end
+
+  def about
+    # render('about')
+  end
+
+  def hello
+    redirect_to(action: 'index')
+  end
+  
 end
